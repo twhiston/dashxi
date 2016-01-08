bf52e4b67338b43ab830e2fb5fc49a22ba89ae15
Tom W <tom.whiston@gmail.com>
2016-01-08 21:43:31 +0100

    update use directives for new twLib namespace structure

--- src/Commands/Export.php
+++ src/Commands/Export.php
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Yaml\Yaml;
-use twhiston\twLib\Str;
+use twhiston\twLib\Str\Str;
 
 
 /**
--- src/Commands/Import.php
+++ src/Commands/Import.php
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Yaml\Parser;
-use twhiston\twLib\Str;
+use twhiston\twLib\Str\Str;
 
 
 /**
